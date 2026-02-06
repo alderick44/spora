428948d9be3de6294f055e7fd618c861a2a41f39
alderick44 <alderick44@hotmail.com>
2026-02-06 11:33:29 -0500

    Fix images links Front-page + Footer

--- footer.php
+++ footer.php
@@ -1,3 +1,75 @@
   <?php wp_footer(); ?>
+      <footer class="border pt-5 bg-light">
+      <div class="container pb-4">
+        <div class="row justify-content-evenly">
+          <div class="col-6 col-sm-4 col-md-3 pb-5">
+            <h4 class="pb-3">Email</h4>
+            <a href="index.html">info@sporacultus.ca</a>
+          </div>
+          <div class="col-6 col-sm-4 col-md-3 pb-5">
+            <h4 class="pb-3">Pages</h4>
+            <div class="d-flex flex-column gap-4">
+              <a href="index.html">Accueil</a>
+              <a href="boutique.html">Boutique</a>
+            </div>
+          </div>
+          <div class="col-6 col-sm-4 col-md-3">
+            <h4 class="pb-3">Suivez-nous</h4>
+            <div class="d-flex flex-column gap-4">
+              <a href="index.html">Facebook</a>
+              <a href="index.html">Instagram</a>
+              <a href="index.html">Twitter</a>
+            </div>
+          </div>
+          <div class="col-6 col-sm-4 col-md-3">
+            <h4 class="pb-3">Produits</h4>
+            <div class="d-flex flex-column gap-4">
+              <a href="mycelium-en-vrac.html">Mycélium en vrac</a>
+            </div>
+          </div>
+          <div class="col-6 col-sm-4 col-md-3">
+            <!--Vide-->
+          </div>
+          <div class="col-6 col-sm-4 col-md-3">
+            <!--Vide-->
+          </div>
+        </div>
+        <div class="d-flex flex-column text-center pt-5 mt-5">
+          <div>
+            <img
+              src="<?php echo get_template_directory_uri();?>/assets/img/logo-transparent.png"
+              class="w-25"
+              alt="logo sporacultus"
+            />
+          </div>
+          <div class="mt-5">
+            <p>© 2026 Sporacultus. Tous droits réservés.</p>
+          </div>
+        </div>
+        <!-- <div class="d-flex w-100 text-center justify-content-center">
+                    <nav class="row g-4 m-2 w-50">
+                        <a href="index.html" class="col-6 col-md-3"><img class="w-50 logo"
+                                src="https://upload.wikimedia.org/wikipedia/commons/b/b9/2023_Facebook_icon.svg"
+                                alt="logo facebook"></a>
+                        <a href="index.html" class="col-6 col-md-3"><img class="w-50 logo"
+                                src="https://cdn.freebiesupply.com/logos/large/2x/linkedin-icon-logo-png-transparent.png"
+                                alt="logo linkedin"></a>
+                        <a href="index.html" class="col-6 col-md-3"><img class="w-50 logo"
+                                src="https://upload.wikimedia.org/wikipedia/commons/6/6f/Logo_of_Twitter.svg"
+                                alt="logo twitter"></a>
+                        <a href="index.html" class="col-6 col-md-3"><img class="w-50 logo"
+                                src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg"
+                                alt="logo instagram"></a>
+                    </nav>
+                </div> -->
+      </div>
+      <div class="pt-5 bg-dark"></div>
+    </footer>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+      crossorigin="anonymous"
+    ></script>
+    <script src="scripts/index.js"></script>
 </body>
 </html>
