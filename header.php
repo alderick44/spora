3081f6b9c80fdad044b92445eb7502ad2312a7d4
alderick44 <alderick44@hotmail.com>
2026-09-06 21:01:18 -0400

    Repare la nav desktop cassee par le fix du menu mobile
    Bootstrap n'a pas de variantes responsives pour les classes de largeur
    (contrairement a d-md-*): w-md-auto n'existait pas, w-100 s'appliquait
    donc a toutes les tailles.

--- header.php
+++ header.php
@@ -110,7 +110,7 @@
 
           <div class="col-md-10 col-12">
             <div class="header-nav d-flex align-items-center justify-content-between justify-content-md-end gap-3">
-              <nav class="header-nav__lists d-flex flex-column align-items-start align-items-md-end w-100 w-md-auto" id="headerMenu">
+              <nav class="header-nav__lists d-flex flex-column align-items-start align-items-md-end w-100" id="headerMenu">
                   <ul class="nav nav-underline flex-wrap gap-3 mb-1 small header-nav__utility d-flex w-100">
                     <li class="nav-item">
                       <a class="nav-link px-2" href="/a-propos/"
